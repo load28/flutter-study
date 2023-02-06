03f510cb7972393a4fb975057dd5a9137a1c3075
load28 <tjalsdud89@naver.com>
2023-02-06 23:10:47 +0900

    init

---
+++ lib/widgets/small_text.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class SmallText extends StatelessWidget {
+  final String text;
+  Color? color;
+  double size;
+  double height;
+
+  SmallText(
+    {
+      Key? key,
+      required this.text,
+      this.color = const Color(0xFFccc7c5),
+      this.size = 12,
+      this.height = 1.2,
+    }
+  ) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontSize: size,
+      ),
+    );
+  }
+}
