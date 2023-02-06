75735931a8c7dfa84f4917155ab113b0eb7e7262
load28 <tjalsdud89@naver.com>
2023-02-06 23:40:06 +0900

    add page view

--- lib/home/main_page_body.dart
+++ lib/home/main_page_body.dart
@@ -11,6 +11,7 @@ class MainPageBodyState extends State<MainPageBody> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.redAccent,
       height: 320,
       child: PageView.builder(
           itemCount: 5,
@@ -21,13 +22,36 @@ class MainPageBodyState extends State<MainPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? Color(0xFF69c5df): Color(0xFF9294cc),
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: index.isEven ? Color(0xFF69c5df): Color(0xFF9294cc),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: AssetImage("assets/images/food_1.jpg"),
+              )
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: EdgeInsets.only(left: 5, right: 5),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: index.isEven ? Color(0xFF69c5df): Color(0xFF9294cc),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/food_1.jpg"),
+                )
+            ),
+          ),
+        )
+      ],
     );
   }
 }
