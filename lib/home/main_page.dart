fbc5adf3493f6dcb2055d2d6fb3f92fe1745dc84
Derek seo <derek@swit.io>
2023-02-08 07:54:34 +0900

    reformat

--- lib/home/main_page.dart
+++ lib/home/main_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/utils/app_colors.dart';
 import 'package:my_app/widgets/big_text.dart';
-import 'package:my_app/widgets/small_text.dart';
 
 import 'main_page_body.dart';
 
@@ -20,36 +19,40 @@ class MyHomeState extends State<MainPage> {
     return Scaffold(
       body: Column(
         children: [
-          Container(
-            child: Container(
-              margin: EdgeInsets.only(top: 45, bottom: 15),
-              padding: EdgeInsets.only(left:20, right: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      BigText(text: "Derek seo", color: AppColors.mainColor),
-                    ],
-                  ),
-                  Center(
-                    child: Container(
-                      width: 45,
-                      height: 45,
-                      child: Icon(Icons.search, color: Colors.white),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: AppColors.mainColor,
-                      ),
-                    ),
-                  )
-                ]
-              )
-            ),
-          ),
+          _header(),
           MainPageBody(),
         ],
       )
+    );
+  }
+
+  Widget _header() {
+    return Container(
+      child: Container(
+        margin: EdgeInsets.only(top: 45, bottom: 15),
+        padding: EdgeInsets.only(left:20, right: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              children: [
+                BigText(text: "Derek seo", color: AppColors.mainColor),
+              ],
+            ),
+            Center(
+              child: Container(
+                width: 45,
+                height: 45,
+                child: Icon(Icons.search, color: Colors.white),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: AppColors.mainColor,
+                ),
+              ),
+            )
+          ]
+        )
+      ),
     );
   }
 }
